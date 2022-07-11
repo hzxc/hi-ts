b47bcc8fc349b88b3d4b8739cd70ee601675c1a7
x <x@x.com>
2022-07-11 23:13:32 +0800

    2022-7-11 23:13

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,17 @@ console.log("index");
 
 const count: number = 23;
 console.log("count:", count);
+
+// import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
+// import { PingPongServiceClient } from "./gen/node/pingpong/v1/pingpong.client";
+
+// const client = new PingPongServiceClient(
+//   new GrpcWebFetchTransport({
+//     baseUrl: "https://tmpl-go-vercel.vercel.app",
+//     format: "text",
+//   })
+// );
+// const call = client.pingPong({ ping: "ping" }, {});
+// call.response.then((resp) => {
+//   console.log(resp.pong);
+// });
