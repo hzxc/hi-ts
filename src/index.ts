cb5d5b48ee7bfe882beb083af9c1a158ed51ddb8
x <x@x.com>
2022-07-13 00:16:06 +0800

    2022-7-13 00:16

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport';
 // import { PingPongServiceClient } from './gen/api/node/pingpong/v1/pingpong.client';
-import { PingPongServiceClient } from './gen/node/api/pingpong/v1/pingpong.client';
+import { HelloServiceClient } from './gen/node/api/hello/v1/hello.client';
 
-const client = new PingPongServiceClient(
+const client = new HelloServiceClient(
   new GrpcWebFetchTransport({
     baseUrl: 'https://tmpl-go-vercel.vercel.app',
     format: 'text',
   })
 );
-const call = client.pingPong({ ping: 'ping' }, {});
+const call = client.intro({ name: 'foo' }, {});
 call.response.then((resp) => {
-  console.log(resp.pong);
+  console.log(resp.intro);
 });
