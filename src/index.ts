239cdcc9508d3bab8205d3847fde1f68da508865
x <x@x.com>
2022-07-14 00:47:07 +0800

    2022-7-14 00:47

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
 import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport';
 // import { PingPongServiceClient } from './gen/api/node/pingpong/v1/pingpong.client';
 import { HelloServiceClient } from './gen/node/api/hello/v1/hello.client';
-
-const client = new HelloServiceClient(
-  new GrpcWebFetchTransport({
-    baseUrl: 'https://tmpl-go-vercel.vercel.app',
-    format: 'text',
-  })
-);
-const call = client.intro({ name: 'foo' }, {});
-call.response.then((resp) => {
-  console.log(resp.intro);
+import { StatusServiceClient } from './gen/node/api/healthcheck/v1/healthcheck.client';
+const trans = new GrpcWebFetchTransport({
+  // baseUrl: 'https://tmpl-go-vercel.vercel.app',
+  baseUrl: 'http://localhost:3000',
+  format: 'text',
 });
+
+const helloClient = new HelloServiceClient(trans);
+const hello = helloClient.intro({ name: 'foo' }, {});
+
+// hello.status
+//   .then((resp) => {
+//     console.log('code', resp.code);
+//     console.log('detail', resp.detail);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+hello.response
+  .then(
+    (resp) => {
+      console.log('resp', resp.intro);
+    }
+    // (rej) => {
+    //   console.log('rej', rej);
+    // }
+  )
+  .catch((err) => {
+    console.log('catch', err);
+  });
+
+// const healthcheckClient = new StatusServiceClient(trans);
+// const status = healthcheckClient.status({}, {});
+// status.response
+//   .then((resp) => {
+//     console.log(resp.version);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
